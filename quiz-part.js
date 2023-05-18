5f13789fa1d51cf88c17571baee6051b53545074
erik-learning <113407204+erik-learning@users.noreply.github.com>
2023-05-18 06:15:14 +0600

    Add files via upload

--- quiz-part.js
+++ quiz-part.js
@@ -1,13 +1,17 @@
-var selectedBottle = 0;  // Initialize with 0 instead of null
-var selectedBottleSrc = "";  // Initialize with "" instead of null
+var selectedBottle = 0;  // Инициализируем с 0 вместо null
 var selectedBottleSrc = "1.png";
+var selectedBottlePrice = document.getElementsByClassName('bottle-option')[0].dataset.price;
+var selectedSkinType = null;
+var selectedAroma = null; 
+var selectedBottleML = document.getElementsByClassName('bottle-option')[0].dataset.ml;
 
 function selectBottle(index) {
     selectedBottle = index;
-    // Update the selected bottle source
-    selectedBottleSrc = document.getElementsByClassName('bottle-option')[index].querySelector('img').src;
-    // Highlight the selected bottle
+    var selectedOption = document.getElementsByClassName('bottle-option')[index];
+    selectedBottleSrc = selectedOption.querySelector('img').src;
+    selectedBottlePrice = selectedOption.dataset.price;
     var bottles = document.getElementsByClassName('bottle-option');
+    selectedBottleML = selectedOption.dataset.ml;
     for (var i = 0; i < bottles.length; i++) {
         if (i === index) {
             bottles[i].classList.add('active');
@@ -15,6 +19,7 @@ function selectBottle(index) {
             bottles[i].classList.remove('active');
         }
     }
+    
 }
 
 function slideBottles(direction) {
@@ -25,69 +30,139 @@ function slideBottles(direction) {
     }
 }
 
-
-
 function validateAndProceed(currentQuestion, nextQuestion) {
-    // Check if the current question is valid
+    // Проверяем, является ли текущий вопрос допустимым
     if (currentQuestion === 1) {
-        // For question 1, check if a bottle is selected
+        // Для вопроса 1 проверяем, выбрана ли бутылка
         if (selectedBottle === null) {
-            document.getElementById('error').innerText = 'Please select a bottle before proceeding.';
+            document.getElementById('error').innerText = 'Пожалуйста, выберите бутылку перед продолжением.';
             return;
         }
     }
     if (currentQuestion === 2) {
-        // For question 2, check if the input field is not empty
+        // Для вопроса 2 проверяем, не является ли поле ввода пустым
         var perfumeName = document.getElementById('perfumeName').value;
         if (!perfumeName) {
-            document.getElementById('error').innerText = 'Please enter a name for your perfume before proceeding.';
+            document.getElementById('error').innerText = 'Пожалуйста, введите название своего парфюма перед продолжением.';
             return;
         }
     }
     if (currentQuestion === 3) {
-        // For question 3, check if a radio button is selected
+        // Для вопроса 3 проверяем, выбрана ли радио-кнопка
         var radios = document.getElementsByName('skin');
         var isChecked = false;
         for (var i = 0; i < radios.length; i++) {
             if (radios[i].checked) {
                 isChecked = true;
+                selectedSkinType = radios[i].value;
                 break;
             }
         }
 
         if (!isChecked) {
-            document.getElementById('error').innerText = 'Please select an option before proceeding.';
+            document.getElementById('error').innerText = 'Пожалуйста, выберите один из вариантов перед продолжением.';
             return;
         }
     }
 
-    // Clear the error message
+    // Очищаем сообщение об ошибке
     document.getElementById('error').innerText = '';
 
-    // Hide the current question
+    // Скрываем текущий вопрос
     document.getElementById('question' + currentQuestion).style.display = 'none';
 
-    // Show the next question or the result
+    // Показываем следующий вопрос или результат
     if (nextQuestion === 'result') {
         showResult();
+    } else if (nextQuestion === 4) {
+        showQuestion4();
     } else {
         document.getElementById('question' + nextQuestion).style.display = 'block';
     }
 }
 
+function showQuestion4() {
+    // Определяем, какие варианты показывать на основе выбранного типа кожи
+    var options;
+    if (selectedSkinType === 'oily') {
+        options = [
+            'Цитрусовые ароматы, такие как лимон, грейпфрут, лайм и бергамот',
+            'Зеленые ароматы, такие как мята, базилик, эстрагон, можжевельник и петитгрейн',
+            'Фруктовые ароматы, такие как яблоко, груша и персик',
+            'Ароматы лаванды и розмарина',
+            'Цветочные ароматы, такие как жасмин, роза и иланг-иланг'
+        ];
+    } else if (selectedSkinType === 'dry') {
+        options = [
+            'Ароматы ванили, кокоса и меда',
+            'Ароматы жасмина, лаванды и розы',
+            'Ароматы молока, овсянки и мандарина',
+            'Ароматы сандала, пачули и ветивера',
+            'Ароматы герани и розмарина',
+        ];
+    } else if (selectedSkinType === 'combination') {
+        options = [
+            'Ароматы бергамота, лимона и грейпфрута',
+            'Ароматы мяты, базилика, можжевельника и петитгрейна',
+            'Ароматы лаванды, розмарина и герани',
+            'Ароматы розы, жасмина и иланг-иланг',
+            'Ароматы кедра, сандала и пачули',
+        ];
+    }
 
+    // Создаем кнопки для каждого варианта
+    var optionsContainer = document.getElementById('options-container');
+    optionsContainer.innerHTML = '';
+    options.forEach(function(option, index) {
+        var btn = document.createElement('button');
+        btn.className = 'aroma-option'; // Добавляем класс кнопке
+        btn.textContent = option;
+        btn.onclick = function() {
+            selectedAroma = option; 
+            // Удаляем класс selected у всех вариантов
+            var options = document.getElementsByClassName('aroma-option');
+            for (var i = 0; i < options.length; i++) {
+                options[i].classList.remove('selected');
+            }
+            // Добавляем класс selected для выбранного варианта
+            this.classList.add('selected');
+        };
+        optionsContainer.appendChild(btn);
+    });
+
+    // Показываем вопрос
+    document.getElementById('question4').style.display = 'block';
+}
 
 function showResult() {
-    // Display the perfume name
-    var perfumeName = document.getElementById('perfumeName').value;
+    // Скрываем все вопросы
+    var questions = document.getElementsByClassName('question');
+    for (var i = 0; i < questions.length; i++) {
+        questions[i].style.display = 'none';
+    }
 
-    // Set the result image source to the selected bottle image
-    document.querySelector('#result img').src = selectedBottleSrc;
-
-    // Set the text on the curved path to the perfume name
-    document.getElementById('curvedText').textContent = perfumeName;
-
-    // Show the result
+    // Показываем результат
     document.getElementById('result').style.display = 'block';
+
+    // Отображаем изображение парфюма
+    var resultImg = document.getElementById('resultImg');
+    console.log(selectedBottleSrc); // Отладка
+    resultImg.src = selectedBottleSrc;
+
+    // Отображаем название парфюма на бутылке
+    var perfumeNameOnBottle = document.getElementById('perfumeNameOnBottle');
+    var perfumeName = document.getElementById('perfumeName').value;
+    perfumeNameOnBottle.textContent = perfumeName;
+
+    // Отображаем описание парфюма
+    var description = document.getElementById('perfumeDescription');
+    description.textContent = 'Вы выбрали бутылку с ароматом ' + selectedAroma + '.';
+
+    // Отображаем цену и объем парфюма
+    var price = document.getElementById('perfumePrice');
+    var ml = document.getElementById('perfumeML');
+    price.textContent = 'Цена: ' + selectedBottlePrice + '₸';
+    ml.textContent = 'Объем: ' + selectedBottleML + ' мл';
 }
+
 
